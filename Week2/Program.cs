cb0dbbe722601c8fdb36806eaa052dd46c553fdc
ksp1510 <42215711+ksp1510@users.noreply.github.com>
2021-05-17 19:34:36 +0530

    Add project files.

---
+++ Week2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 454;
+            int sq = num * num; 
+            Console.WriteLine("The square of {0} is {1}", num, sq);
+            Console.WriteLine("-----------------------------------------------------------");
+            int a = 5, b = 9, c = 10, d = 7;
+            int add = a + b + c + d;
+            Console.WriteLine("The addition of {0}, {1}, {2} and {3} is {4}", a, b, c, d, add);
+            Console.Read();
+
+        }
+    }
+}
